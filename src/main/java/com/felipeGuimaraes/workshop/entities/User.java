ed446add334078caf3fac0b1b826e939336052fd
Felipe Guimaraes1 <felipe.guimaraes1@ibm.com>
2024-01-09 17:25:59 -0300

    feat: H2 database, test profile, JPA

--- src/main/java/com/felipeGuimaraes/workshop/entities/User.java
+++ src/main/java/com/felipeGuimaraes/workshop/entities/User.java
@@ -1,5 +1,6 @@
 package com.felipeGuimaraes.workshop.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
@@ -9,10 +10,14 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID =1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
