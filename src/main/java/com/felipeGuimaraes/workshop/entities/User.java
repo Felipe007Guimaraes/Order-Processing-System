5088ad2f2f9ba2987c41e4b980227b23a30eb9f8
Felipe Guimaraes1 <felipe.guimaraes1@ibm.com>
2024-01-11 18:09:54 -0300

    feat: Order, Instant, ISO 8601

--- src/main/java/com/felipeGuimaraes/workshop/entities/User.java
+++ src/main/java/com/felipeGuimaraes/workshop/entities/User.java
@@ -1,15 +1,15 @@
 package com.felipeGuimaraes.workshop.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
@@ -23,4 +23,15 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+    public User (Long id, String name, String email, String phone, String password){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
 }
