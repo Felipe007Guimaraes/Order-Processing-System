8b8afb97b5e632b4feeb157790b7caf402af727f
Felipe007Guimaraes <felipe007.dnl123@gmail.com>
2024-01-13 17:21:27 -0300

    feat: Category

--- src/main/java/com/felipeGuimaraes/workshop/resources/CategoryResource.java
+++ src/main/java/com/felipeGuimaraes/workshop/resources/CategoryResource.java
@@ -1,7 +1,7 @@
 package com.felipeGuimaraes.workshop.resources;
 
-import com.felipeGuimaraes.workshop.entities.User;
-import com.felipeGuimaraes.workshop.service.UserService;
+import com.felipeGuimaraes.workshop.entities.Category;
+import com.felipeGuimaraes.workshop.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
+@RequestMapping(value = "/categories")
+public class CategoryResource {
 
     @Autowired
-    private UserService service;
+    private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
-        List<User> list = service.findAll();
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id) {
-        User user = service.findById(id);
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+        Category category = service.findById(id);
+        return ResponseEntity.ok().body(category);
     }
 }
